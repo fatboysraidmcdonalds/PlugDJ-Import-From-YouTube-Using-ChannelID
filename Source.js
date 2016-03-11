101ade26444b6cbd0a4a87714eec8935aecc9433
fatboysraidmcdonalds <refatboysraidmcdonalds@gmail.com>
2016-03-10 23:48:56 -0500

    Source

---
+++ Source.js
@@ -0,0 +1,8 @@
+/*https://www.reddit.com/r/plugdj/comments/3ixh34/yet_another_youtube_import_hack_for_those_with/*/
+window.gapi.client.youtube.channels.yoba = window.gapi.client.youtube.channels.list;
+  window.gapi.client.youtube.channels.list = function(params){ 
+  params["id"] = params["forUsername"];
+  params["forUsername"] = null;
+  return window.gapi.client.youtube.channels.yoba(params);
+}
+$('#youtube-input-field').attr("placeholder","Youtube ChannelId");
